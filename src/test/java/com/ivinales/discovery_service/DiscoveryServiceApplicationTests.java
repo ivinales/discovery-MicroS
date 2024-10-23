8e6354152b2b451ad0a05441e9e80088a25bfa4e
ivinales <ian.vinales26@gmail.com>
2024-10-22 23:16:46 -0300

    eureka primera configuraciones

---
+++ src/test/java/com/ivinales/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivinales.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
